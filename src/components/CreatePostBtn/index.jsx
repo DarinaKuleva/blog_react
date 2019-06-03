b2d7d6ce3f9a7d6f50e92f1dd06f6e8a94ad2948
Darina <darinakuleva@gmail.com>
2019-06-03 17:39:02 +0300

    ISSUE-6 Стилизация шапки

---
+++ src/components/CreatePostBtn/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import button from './style.module.css'
+
+const CreatePostButton = () => {
+  return (
+    <>
+      <button className={button.page}>
+        Create new post
+      </button>
+    </>
+  )
+}
+
+export default CreatePostButton
--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -4,13 +4,16 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { postsFetchData } from '../../actions/postsFetch'
 import ViewCommentsButton from '../ViewCommentsBtn'
+import CreatePostBtn from '../CreatePostBtn'
+
+import blog from './style.module.css'
 
 class Posts extends PureComponent {
 
   static propTypes = {
     posts: PropTypes.array.isRequired, //проверить все ли пропсы+подчеркивание
     hasErrored: PropTypes.bool.isRequired,
-    isLoading: PropTypes.bool.isRequired
+    isLoading: PropTypes.bool.isRequired,
   }
 
   componentDidMount() {
@@ -35,17 +38,32 @@ class Posts extends PureComponent {
       return <p>Loading…</p>
     }
     return (
-      <ul>
-        { posts.map( ( post ) => (
-          <li key={ post.id }>
-            <Link to={ `post-information/${ post.id }` }>
-              <h2>{ post.title }</h2>
-              <p>{ post.body }</p>
-            </Link>
-            <ViewCommentsButton commentId={ post.id }/>
-          </li>
-        ) ) }
-      </ul>
+      <section className={blog.container}>
+        <div className={blog.header}>
+          <h1 className={blog.logo}>Blog</h1>
+          <CreatePostBtn/>
+        </div>
+        <div className={blog.sorting}>
+          <button className={blog.sorting__item}>По алфавиту</button>
+          <button className={blog.sorting__item}>По лайкам</button>
+          <button className={blog.sorting__item}>По дизлайкам</button>
+          <button className={blog.sorting__item}>Сбросить сортировку</button>
+        </div>
+        <div>
+          <input placeholder="Искать" className={blog.search}/>
+        </div>
+        <ul>
+          { posts.map( ( post ) => (
+            <li key={ post.id }>
+              <Link to={ `post-information/${ post.id }` }>
+                <h2>{ post.title }</h2>
+                <p>{ post.body }</p>
+              </Link>
+              <ViewCommentsButton commentId={ post.id }/>
+            </li>
+          ) ) }
+        </ul>
+      </section>
     )
   }
 }
