5737bb64cd698a865e50c4d16244719b4abd150e
Darina <darinakuleva@gmail.com>
2019-06-13 14:35:55 +0300

    ISSUE-25 Добавление валидации нового поста

--- src/components/CreateNewPost/index.jsx
+++ src/components/CreateNewPost/index.jsx
@@ -2,55 +2,102 @@ import React from 'react'
 import { connect } from 'react-redux'
 import addNewPost from '../../actions/addNewPost'
 import { Link } from 'react-router-dom'
+import FormErrors from '../FormError'
 
 class CreateNewPost extends React.PureComponent {
 
   state = {
     title: '',
     body: '',
+    formErrors: { title: '', body: '' },
+    postTitleValid: false,
+    postBodyValid: false,
+    formValid: false,
   }
 
   render() {
     return (
-      <div>
-        <input
-          placeholder="Call your post..."
-          type="text"
-          value={ this.setState.title }
-          onChange={ this.handleChangePostTitle }
-        />
-        <textarea
-          placeholder="Write your post ..."
-          value={ this.state.body }
-          onChange={ this.handleChangePost }
-        />
-        <Link to="/">
-          <button
-            onClick={ this.createPost }
-          >
-            Publish
-          </button>
-        </Link>
+      <>
+        <form>
+          <h2>Sign up</h2>
+          <div>
+            <label htmlFor="title">Call your post...</label>
+            <input name="title"
+                   placeholder="Title..."
+                   type="text"
+                   value={ this.setState.title }
+                   onChange={ this.handleUserInput }
+                   required/>
+          </div>
+          <div>
+            <label htmlFor="body">Write your post ...</label>
+            <textarea type="text"
+                      name="body"
+                      placeholder="Post..."
+                      value={ this.setState.title }
+                      onChange={ this.handleUserInput }/>
+          </div>
+          <Link to="/">
+            <button
+              onClick={ this.createPost }
+              disabled={ !this.state.formValid }
+            >
+              Publish
+            </button>
+          </Link>
+        </form>
+        <div>
+          <FormErrors formErrors={ this.state.formErrors }/>
+        </div>
         <Link to="/">
           POSTS
         </Link>
-      </div>
+      </>
     )
   }
 
-  handleChangePostTitle = e => {
-    this.setState( { title: e.target.value } )
+  handleUserInput = ( e ) => {
+    const name = e.target.name
+    const value = e.target.value
+    this.setState( { [name]: value },
+      () => {
+        this.validateField( name, value )
+      } )
   }
 
-  handleChangePost = e => {
-    this.setState( { body: e.target.value } )
+  validateField( fieldName, value ) {
+    let fieldValidationErrors = this.state.formErrors
+    let postTitleValid = this.state.postTitleValid
+    let postBodyValid = this.state.postBodyValid
+
+    switch ( fieldName ) {
+      case 'title':
+        // emailValid = value.match( /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i )
+        postTitleValid = value.length >= 3
+        fieldValidationErrors.postTitle = postTitleValid ? '' : 'Title is too short'
+        break
+      case 'body':
+        postBodyValid = value.length >= 20
+        fieldValidationErrors.postBody = postBodyValid ? '' : 'Body is too short'
+        break
+      default:
+        break
+    }
+    this.setState( {
+      formErrors: fieldValidationErrors,
+      postTitleValid: postTitleValid,
+      postBodyValid: postBodyValid,
+    }, this.validateForm )
+  }
+
+  validateForm() {
+    this.setState( { formValid: this.state.postTitleValid && this.state.postBodyValid } )
   }
 
   createPost = () => {
     const postTitle = this.state.title
     const postBody = this.state.body
     this.props.addNewPost( postTitle, postBody )
-    this.setState( { title: '', body: '' } )
   }
 }
 
---
+++ src/components/FormError/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FormErrors = ({formErrors}) =>
+  <div>
+    {Object.keys(formErrors).map((fieldName, errorNumber) => {
+      if (formErrors[fieldName].length > 0){
+        return (
+          <p key={errorNumber}>{formErrors[fieldName]}</p>
+        )
+      } else {
+        return '';
+      }
+    })}
+  </div>
+export default FormErrors
